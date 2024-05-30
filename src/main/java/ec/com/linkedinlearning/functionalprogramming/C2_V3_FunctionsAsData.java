d1856eddea17c1cb12108c8f0bf05f77791d8195
Steven <stevengf14@gmail.com>
2024-05-29 22:18:06 -0500

    Functions as data

---
+++ src/main/java/ec/com/linkedinlearning/functionalprogramming/C2_V3_FunctionsAsData.java
@@ -0,0 +1,51 @@
+package ec.com.linkedinlearning.functionalprogramming;
+
+/**
+ *
+ * @author Steve
+ */
+public class C2_V3_FunctionsAsData {
+
+    protected static class Person {
+
+        private String name;
+        private Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" + "name=" + name + ", age=" + age + '}';
+        }
+
+    }
+
+    protected static class DataLoader {
+
+        public final NoArgFunction<Person> loadPerson;
+
+        public DataLoader(Boolean isDevelopment) {
+            this.loadPerson = isDevelopment ? this::loadPersonFake : this::loadPersonReal;
+        }
+
+        private Person loadPersonReal() {
+            System.out.println("Loading person...");
+            return new Person("Real Person", 30);
+        }
+
+        private Person loadPersonFake() {
+            System.out.println("Returning fake person object...");
+            return new Person("Fake Person", 100);
+        }
+    }
+
+    public static void main(String[] args) {
+        final Boolean IS_DEVELOPMENT = true;
+        DataLoader dataLoader = new DataLoader(IS_DEVELOPMENT);
+        System.out.println(dataLoader.loadPerson.apply());
+    }
+
+}
