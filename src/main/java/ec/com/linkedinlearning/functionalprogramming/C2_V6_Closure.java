67b3135b02e3f44366673a9a905a93bfcccbfb0d
Steven <stevengf14@gmail.com>
2024-05-30 22:28:14 -0500

    Closure

---
+++ src/main/java/ec/com/linkedinlearning/functionalprogramming/C2_V6_Closure.java
@@ -0,0 +1,19 @@
+package ec.com.linkedinlearning.functionalprogramming;
+
+/**
+ *
+ * @author Steve
+ */
+public class C2_V6_Closure {
+
+    public static void main(String[] args) {
+        NoArgFunction<NoArgFunction<String>> createGreeter = () -> {
+            String name = "Steven";
+            return () -> "Hello " + name;
+        };
+
+        NoArgFunction<String> greeter = createGreeter.apply();
+        System.out.println(greeter.apply());
+    }
+
+}
