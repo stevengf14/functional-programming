971cca3dd355b761ebfc740bb57aa40295272e3c
Steven <stevengf14@gmail.com>
2024-06-17 22:20:08 -0500

    Combine list functions

---
+++ src/main/java/ec/com/linkedinlearning/functionalprogramming/C3_V7_CombineListFunctions.java
@@ -0,0 +1,54 @@
+package ec.com.linkedinlearning.functionalprogramming;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Steve
+ */
+public class C3_V7_CombineListFunctions {
+
+    static class Employee {
+
+        public final String name;
+        public final Integer age;
+        public final String jobTitle;
+        public final Float salary;
+
+        public Employee(String name, Integer age, String jobTitle, Float salary) {
+            this.name = name;
+            this.age = age;
+            this.jobTitle = jobTitle;
+            this.salary = salary;
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee[] employeesArr = {
+            new Employee("John", 34, "developer", 80000f),
+            new Employee("Hannah", 24, "developer", 95000f),
+            new Employee("Bart", 50, "sales executive", 100000f),
+            new Employee("Sophie", 49, "construction worker", 40000f),
+            new Employee("Darren", 38, "writer", 50000f),
+            new Employee("Nancy", 29, "developer", 75000f),};
+        List<Employee> employees = new ArrayList<>(Arrays.asList(employeesArr));
+
+        Float totalDeveloperSalaries = employees
+                .stream()
+                .filter((employee) -> employee.jobTitle.equals("developer"))
+                .map(developer -> developer.salary)
+                .reduce(0f, (acc, x) -> acc + x);
+
+        Long numberOfDevelopers = employees
+                .stream()
+                .filter(employee -> employee.jobTitle.equals("developer"))
+                .collect(Collectors.counting());
+
+        Float averageDeveloperSalary = totalDeveloperSalaries / numberOfDevelopers;
+
+        System.out.println(averageDeveloperSalary);
+    }
+}
