6599e29845f46168346e971391c4dabb9e298da7
Steven <stevengf14@gmail.com>
2024-06-19 22:49:35 -0500

    challenge

---
+++ src/main/java/ec/com/linkedinlearning/functionalprogramming/C4_Challenge.java
@@ -0,0 +1,20 @@
+package ec.com.linkedinlearning.functionalprogramming;
+
+/**
+ *
+ * @author Steve
+ */
+public class C4_Challenge {
+
+    static int fibonacci(int x) {
+        // Your code goes here
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The 5th fibonacci number is: " + fibonacci(5));
+        System.out.println("The 10th fibonacci number is: " + fibonacci(10));
+        System.out.println("The 17th fibonacci number is: " + fibonacci(17));
+    }
+}
